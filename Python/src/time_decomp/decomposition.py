73812c50a3a5154e340cf1f2292a88b092ccd4fb
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-09-17 11:03:36 +0300

    Update decomposition.py

--- Python/src/time_decomp/decomposition.py
+++ Python/src/time_decomp/decomposition.py
@@ -1,5 +1,6 @@
 # add class DecompositionSingleton
 import pandas as pd
+import statsmodels.api as sm
 
 class DecompositionSingleton:
     _instance = None
@@ -12,5 +13,15 @@ class DecompositionSingleton:
     def __init__(self):
         if not hasattr(self, 'initialized'):
             self.df = pd.DataFrame()
+            self.features = []
+            self.s = {}
+            self.decompose_params = {}
             self.initialized = True
+
+    def m_decompose(self):
+        # from dictionary self.decompose_params, get the parameters for seasonal decomposition
+        # apply seasonal decomposition on each feature in self.features
+        for c in self.features:
+            self.s[c] = sm.tsa.seasonal_decompose(self.df.loc[:,c], **self.decompose_params)
+
 
