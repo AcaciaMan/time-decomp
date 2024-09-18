4915e1712d382eccee3af674c3f8d31782b19358
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-09-18 05:54:06 +0300

    Test plot something

--- Python/tests/decomposition_test.py
+++ Python/tests/decomposition_test.py
@@ -6,16 +6,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from src.time_decomp.decomposition import DecompositionSingleton
 from statsmodels.tsa.seasonal import DecomposeResult
-import logging
+
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
+print("Configuring logging")
 
 class TestDecompositionSingleton(unittest.TestCase):
     def setUp(self):
         self.decomp = DecompositionSingleton()
-        self.decomp.df = pd.DataFrame({'A': np.random.randn(100), 'B': np.random.randn(100)})
+
+        self.decomp.df = pd.DataFrame({'A': np.random.randint(0,100, size=(100,)), 'B': np.random.randint(0,100, size=(100,))})
+        
         self.decomp.features = ['A', 'B']
         self.decomp.decompose_params = {'model': 'additive', 'period':12, 'extrapolate_trend':'freq'}
         self.decomp.plot_params = {'title': 'Decomposition of feature', 'xlabel': 'Time', 'ylabel': 'Feature'}
@@ -42,14 +43,21 @@ class TestDecompositionSingleton(unittest.TestCase):
         self.assertEqual(len(self.decomp.get_colors_array(20)), 20)
 
     def test_plot_decomposition(self):
-        # output df info
-        logging.debug("DataFrame Info:")
-        #logging.debug(self.decomp.df.info())
-        logging.debug("DataFrame Head:")
-        #logging.debug("\n%s", self.decomp.df.head())
 
-        
+        # output df info
+        print("Starting test_plot_decomposition")
+        print("DataFrame Info:")
+        print(self.decomp.df.info())
+        print("DataFrame Head:")
+        print("\n%s", self.decomp.df.head())
+
+    
         self.decomp.m_decompose()
         self.decomp.plot_decomposition('A', 'A', range(10), 'A', 'A')
         self.decomp.plot_decomposition('B', 'B', 'B', 'B', 'B')
         plt.show()
+
+
+
+if __name__ == '__main__':
+    unittest.main()        
